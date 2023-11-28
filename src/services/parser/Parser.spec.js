730ec1f62636e1a14470fbfc9cfdbf8719b641da
Antti Kuparinen <akuparinen@iki.fi>
2023-11-29 00:04:06 +0200

    Add tests for structure pragma

--- src/services/parser/Parser.spec.js
+++ src/services/parser/Parser.spec.js
@@ -11,14 +11,21 @@ describe("Parser", function () {
             const parser = new Parser('');
             const text = "#title Hello\n";
 
-            expect(parser.parse(text).title).to.equal('Hello');
+            expect(parser.parse(text).metadata.title).to.equal('Hello');
         });
 
         it("right", function () {
             const parser = new Parser('');
             const text = "#right Hello\n";
 
-            expect(parser.parse(text).rightTitle).to.equal('Hello');
+            expect(parser.parse(text).metadata.rightTitle).to.equal('Hello');
+        });
+
+        it("struct", function () {
+            const parser = new Parser('');
+            const text = "#structure A|B|A|B|C|B|B\n";
+
+            expect(parser.parse(text).metadata.structure).to.deep.equal(['A', 'B', 'A', 'B', 'C', 'B', 'B']);
         });
 
     });
